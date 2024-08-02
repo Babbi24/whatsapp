780f9ad2135198d97671a5b8bcbad8599d63c47b
Babbi24 <babbi113@gmail.com>
2024-08-02 15:14:17 +0530

    Add files via upload

---
+++ whatsapp Automation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pywhatkit as kit
+
+
+# In[ ]:
+
+
+# Parameters:
+   # phone number (with country code, e.g., "+1234567890")
+   # message
+   # hour (24-hour format)
+   # minute
+
+phone_number = "+919550787234"
+message = "Hello, this is an automated message!"
+hour = 15 # 10 PM
+minute = 15
+try:
+    kit.sendwhatmsg(phone_number, message, hour, minute)
+    print("Message scheduled successfully!")
+except Exception as e:
+       print(f"An error occurred: {e}")
+
+
+# In[ ]:
+
+
+
+
